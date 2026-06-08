b0c7aa0416e8a654ffa3bf10d782b1f294fe3560
Yarovskiy Andrii <yarovskiy@gmail.com>
2026-06-08 20:50:58 +0200

    solution

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -5,6 +5,13 @@
  * Now it has a basic structure, that is needed for testing.
  * Feel free to add more props and methods if needed.
  */
+const startState = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+];
+
 class Game {
   /**
    * Creates a new game instance.
@@ -21,24 +28,321 @@ class Game {
    * initial state.
    */
   constructor(initialState) {
+    if (!initialState) {
+      this.currentState = structuredClone(startState);
+    } else {
+      this.currentState = initialState;
+    }
     // eslint-disable-next-line no-console
-    console.log(initialState);
+    // console.log(initialState);
+
+    this.score = 0;
+    this.status = 'idle';
+
+    this.value1 = 2;
+    this.value2 = 4;
+
+    // compare fild version
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  // #region moveUp
+  moveUp() {
+    let V1 = '';
+    let V2 = '';
+
+    // // eslint-disable-next-line no-console
+    // console.log(`it's moveUp |`);
+
+    V1 = JSON.stringify(this.currentState);
+
+    for (let i = 0; i < this.currentState.length; i++) {
+      const temp = [];
+
+      for (let j = 0; j < this.currentState[i].length; j++) {
+        temp.push(this.currentState[j][i]);
+      }
+
+      const withoutZeros = [];
+
+      for (let k = 0; k < temp.length; k++) {
+        if (temp[k] !== 0) {
+          withoutZeros.push(temp[k]);
+        }
+      }
+
+      while (withoutZeros.length < temp.length) {
+        withoutZeros.push(0);
+      }
+
+      for (let x = 0; x < startState.length - 1; x++) {
+        if (withoutZeros[x] === withoutZeros[x + 1] && withoutZeros[x] !== 0) {
+          withoutZeros[x] = withoutZeros[x] * 2;
+          this.score += withoutZeros[x];
+          withoutZeros[x + 1] = 0;
+        }
+      }
+
+      const res = [];
+
+      for (let y = 0; y < withoutZeros.length; y++) {
+        if (withoutZeros[y] !== 0) {
+          res.push(withoutZeros[y]);
+        }
+      }
+
+      while (res.length < temp.length) {
+        res.push(0);
+      }
+
+      for (let s = 0; s < withoutZeros.length; s++) {
+        this.currentState[s][i] = res[s];
+      }
+    }
+
+    V2 = JSON.stringify(this.currentState);
+
+    if (V1 === V2) {
+      return false;
+    }
+
+    return true;
+  }
+
+  // #endregion
+
+  // #region moveDown
+  moveDown() {
+    let V1 = '';
+    let V2 = '';
+
+    // eslint-disable-next-line no-console
+    console.log(`it's moveDown |`);
+
+    V1 = JSON.stringify(this.currentState);
+
+    for (let i = 0; i < this.currentState.length; i++) {
+      const temp = [];
+
+      for (let j = this.currentState[i].length - 1; j >= 0; j--) {
+        temp.push(this.currentState[j][i]);
+      }
+
+      const withoutZeros = [];
+
+      for (let k = 0; k < temp.length; k++) {
+        if (temp[k] !== 0) {
+          withoutZeros.push(temp[k]);
+        }
+      }
+
+      // adding zeros
+
+      while (withoutZeros.length < temp.length) {
+        withoutZeros.push(0);
+      }
+
+      // I add the same values
+
+      for (let x = 0; x < startState.length - 1; x++) {
+        if (withoutZeros[x] === withoutZeros[x + 1] && withoutZeros[x] !== 0) {
+          withoutZeros[x] = withoutZeros[x] * 2;
+          this.score += withoutZeros[x];
+          withoutZeros[x + 1] = 0;
+        }
+      }
+
+      const res = [];
+
+      for (let y = 0; y < withoutZeros.length; y++) {
+        if (withoutZeros[y] !== 0) {
+          res.push(withoutZeros[y]);
+        }
+      }
+
+      while (res.length < temp.length) {
+        res.push(0);
+      }
+
+      for (let s = 0; s <= withoutZeros.length - 1; s++) {
+        const nInd = withoutZeros.length - 1 - s;
+
+        this.currentState[nInd][i] = res[s];
+      }
+    }
+
+    V2 = JSON.stringify(this.currentState);
+
+    if (V1 === V2) {
+      return false;
+    }
+
+    return true;
+  }
+  // #endregion
+
+  // #region move left
+
+  moveLeft() {
+    let V1 = '';
+    let V2 = '';
+
+    V1 = JSON.stringify(this.currentState);
+
+    // creating copies of each line
+    for (let i = 0; i < this.currentState.length; i++) {
+      const temp = [];
+
+      for (let j = 0; j < this.currentState[i].length; j++) {
+        temp.push(this.currentState[i][j]);
+      }
+
+      // line without zeros
+
+      const withoutZeros = [];
+
+      for (let k = 0; k < temp.length; k++) {
+        if (temp[k] !== 0) {
+          withoutZeros.push(temp[k]);
+        }
+      }
+
+      // adding zeros
+
+      while (withoutZeros.length < temp.length) {
+        withoutZeros.push(0);
+      }
+
+      // I add the same values
+
+      for (let x = 0; x < startState.length - 1; x++) {
+        if (withoutZeros[x] === withoutZeros[x + 1] && withoutZeros[x] !== 0) {
+          withoutZeros[x] = withoutZeros[x] * 2;
+          this.score += withoutZeros[x];
+          withoutZeros[x + 1] = 0;
+        }
+      }
+
+      // make result masive
+
+      const res = [];
+
+      for (let y = 0; y < withoutZeros.length; y++) {
+        if (withoutZeros[y] !== 0) {
+          res.push(withoutZeros[y]);
+        }
+      }
+
+      // add 0s
+      while (res.length < temp.length) {
+        res.push(0);
+      }
+
+      for (let s = 0; s <= res.length - 1; s++) {
+        this.currentState[i][s] = res[s];
+      }
+
+      // eslint-disable-next-line no-console
+      console.log('this.score-->>', this.score);
+    }
+
+    V2 = JSON.stringify(this.currentState);
+
+    if (V1 === V2) {
+      return false;
+    }
+
+    return true;
+  }
+
+  // #endregion
+
+  // #region move Right
+
+  moveRight() {
+    let V1 = '';
+    let V2 = '';
+
+    V1 = JSON.stringify(this.currentState);
+
+    for (let i = 0; i < this.currentState.length; i++) {
+      const temp = [];
+
+      for (let j = this.currentState[i].length - 1; j >= 0; j--) {
+        temp.push(this.currentState[i][j]);
+      }
+
+      // line without zeros
+
+      const withoutZeros = [];
+
+      for (let k = 0; k < temp.length; k++) {
+        if (temp[k] !== 0) {
+          withoutZeros.push(temp[k]);
+        }
+      }
+
+      // adding zeros
+
+      while (withoutZeros.length < temp.length) {
+        withoutZeros.push(0);
+      }
+
+      // I add the same values
+
+      for (let x = 0; x < startState.length - 1; x++) {
+        if (withoutZeros[x] === withoutZeros[x + 1] && withoutZeros[x] !== 0) {
+          withoutZeros[x] = withoutZeros[x] * 2;
+          this.score += withoutZeros[x];
+          withoutZeros[x + 1] = 0;
+        }
+      }
+
+      // make result masive
+
+      const res = [];
+
+      for (let y = 0; y < withoutZeros.length; y++) {
+        if (withoutZeros[y] !== 0) {
+          res.push(withoutZeros[y]);
+        }
+      }
+
+      // add 0s
+      while (res.length < temp.length) {
+        res.push(0);
+      }
+
+      for (let s = 0; s <= withoutZeros.length - 1; s++) {
+        const nInd = withoutZeros.length - 1 - s;
+
+        this.currentState[i][nInd] = res[s];
+      }
+    }
+
+    V2 = JSON.stringify(this.currentState);
+
+    if (V1 === V2) {
+      return false;
+    }
+
+    return true;
+  }
+
+  // #endregion
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() {
+    return this.score;
+  }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    return this.currentState;
+  }
 
   /**
    * Returns the current game status.
@@ -50,19 +354,103 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() {
+    return this.status;
+  }
 
   /**
    * Starts the game.
    */
-  start() {}
+  start() {
+    this.status = 'playing';
+
+    // // eslint-disable-next-line no-console
+    // console.log('it is inner method START');
+
+    // eslint-disable-next-line no-console
+    // console.log(this.currentState);
+
+    this.generator();
+  }
 
   /**
    * Resets the game.
    */
-  restart() {}
+  restart() {
+    this.currentState = structuredClone(startState);
+    this.score = 0;
+    this.status = 'idle';
+    this.state = 'ready';
+  }
 
   // Add your own methods here
+  findWin() {
+    if (this.currentState.flat().includes(2048)) {
+      this.status = 'win';
+    }
+  }
+
+  generator() {
+    let chosenValue = 0;
+
+    chosenValue = Math.random() < 0.9 ? this.value1 : this.value2;
+
+    const makeFlat = () => {
+      const rez = [];
+
+      for (let i = 0; i < this.currentState.length; i++) {
+        for (let j = 0; j < this.currentState[i].length; j++) {
+          if (this.currentState[i][j] === 0) {
+            rez.push([i, j]);
+          }
+        }
+      }
+
+      return rez;
+    };
+
+    const flatMas = makeFlat();
+
+    if (flatMas.length > 0) {
+      const random = Math.floor(Math.random() * flatMas.length);
+      const ind = flatMas[random];
+
+      this.currentState[ind[0]][ind[1]] = chosenValue;
+      this.V2 = JSON.stringify(this.currentState);
+
+      return this.currentState;
+    } else {
+      return null;
+    }
+  }
+
+  hasMoves() {
+    if (this.currentState.flat().includes(0)) {
+      return true;
+    }
+
+    for (let x = 0; x < this.currentState.length; x++) {
+      for (let y = 0; y < this.currentState.length; y++) {
+        if (
+          y + 1 < this.currentState.length &&
+          this.currentState[x][y] === this.currentState[x][y + 1]
+        ) {
+          return true;
+        }
+
+        if (
+          x + 1 < this.currentState.length &&
+          this.currentState[x][y] === this.currentState[x + 1][y]
+        ) {
+          return true;
+        }
+      }
+    }
+
+    this.status = 'lose';
+
+    return false;
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,125 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
+
+const field = document.querySelectorAll('td');
 
 // Write your code here
+
+// #region push start listner
+
+const pushStart = document.querySelector('.container');
+const start = document.querySelector('.start');
+const masageStart = document.querySelector('.message-start');
+const gameScore = document.querySelector('.game-score');
+const lose = document.querySelector('.message-lose');
+const win = document.querySelector('.message-win');
+
+pushStart.addEventListener('click', (e) => {
+  if (
+    e.target.classList.contains('start') ||
+    e.target.classList.contains('restart')
+  ) {
+    game.restart();
+    game.start();
+
+    render();
+    score();
+
+    start.textContent = 'Restart';
+    start.classList.add('restart');
+    start.classList.remove('start');
+    start.style.fontSize = '17px';
+    masageStart.classList.add('hidden');
+    win.classList.add('hidden');
+    lose.classList.add('hidden');
+  }
+});
+
+// #endregion
+
+// #region press the move.
+function permissionToMove() {
+  if (!game.hasMoves()) {
+    lose.classList.remove('hidden');
+    // lose.classList.add('message-lose');
+  }
+}
+
+function checkWin() {
+  if (game.status === 'win') {
+    win.classList.remove('hidden');
+  }
+}
+
+function makeMove() {
+  game.findWin();
+  game.generator();
+  render();
+  score();
+  checkWin();
+  permissionToMove();
+}
+
+document.addEventListener('keydown', (e) => {
+  if (game.status === 'playing' || game.status === 'win') {
+    if (e.key === 'ArrowUp') {
+      e.preventDefault();
+
+      if (game.moveUp()) {
+        makeMove();
+      }
+    } else if (e.key === 'ArrowDown') {
+      e.preventDefault();
+
+      if (game.moveDown()) {
+        makeMove();
+      }
+    } else if (e.key === 'ArrowLeft') {
+      e.preventDefault();
+
+      if (game.moveLeft()) {
+        makeMove();
+      }
+    } else if (e.key === 'ArrowRight') {
+      e.preventDefault();
+
+      if (game.moveRight()) {
+        makeMove();
+      }
+    }
+  }
+});
+
+// #endregion
+
+// #region render
+
+function score() {
+  gameScore.textContent = game.score;
+}
+
+function render() {
+  let count = 0;
+  const state = game.getState();
+
+  for (let ind = 0; ind < state.length; ind++) {
+    for (let inind = 0; inind < state[ind].length; inind++) {
+      field[count].className = 'field-cell';
+
+      if (state[ind][inind] !== 0) {
+        field[count].textContent = state[ind][inind];
+
+        field[count].classList.add(`field-cell--${state[ind][inind]}`);
+      } else {
+        field[count].textContent = '';
+      }
+
+      count++;
+    }
+  }
+}
+
+// #endregion
